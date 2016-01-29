5343dd7916535787b246a3f5e7a17f284009de33
Carl <carl@carlossus.com>
2016-01-29 21:16:35 +0800

    Use an options object and implement interval guard logic

--- dist/jq-counter.js
+++ dist/jq-counter.js
@@ -1,26 +1,30 @@
 var Counter = (function () {
-    function Counter(element, counterLimit, incrementMs) {
+    function Counter(options) {
         var _this = this;
-        this.element = element;
-        this.counterLimit = counterLimit;
-        this.incrementMs = incrementMs;
+        this.options = options;
+        this.increment = 1;
         this.counter = 0;
         this.start = function () {
+            // Do not allow multiple intervals to run concurrently
+            _this.stop();
             var tick = function () {
-                if (_this.counter === _this.counterLimit) {
+                if (_this.counter === _this.options.end) {
                     return _this.stop();
                 }
-                _this.counter++;
+                _this.counter += _this.increment;
                 _this.update();
             };
-            _this.timer = setInterval(tick, _this.incrementMs);
+            _this.timer = setInterval(tick, _this.options.intervalMs);
         };
         this.stop = function () {
             clearInterval(_this.timer);
         };
         this.update = function () {
-            _this.element.innerHTML = "<span>" + _this.counter + "</span>";
+            _this.options.element.innerHTML = "<span>" + _this.counter + "</span>";
         };
+        this.increment = this.options.start > this.options.end
+            ? -1
+            : 1;
     }
     return Counter;
 }());
--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,34 @@
 class Counter {
-    constructor(public element: HTMLElement, public counterLimit, public incrementMs: number) {
+    constructor(public options: { element: HTMLElement, start: number, end: number, intervalMs: number }) {
+        this.increment = this.options.start > this.options.end
+            ? -1
+            : 1;
     }
-    
+
+    increment = 1;
     counter: number = 0;
     timer: number;
-    
+
     start = () => {
+        
+        // Do not allow multiple intervals to run concurrently
+        this.stop();
+        
         var tick = () => {
-            if (this.counter === this.counterLimit) {
+            if (this.counter === this.options.end) {
                 return this.stop();
             }
-            
-            this.counter++;            
+            this.counter += this.increment;
             this.update();
         }
-        this.timer = setInterval(tick, this.incrementMs)
+        this.timer = setInterval(tick, this.options.intervalMs)
     }
-    
-    
-    
+
     stop = () => {
         clearInterval(this.timer);
     }
-    
+
     update = () => {
-        this.element.innerHTML = `<span>${this.counter}</span>`;
+        this.options.element.innerHTML = `<span>${this.counter}</span>`;
     }
 }
